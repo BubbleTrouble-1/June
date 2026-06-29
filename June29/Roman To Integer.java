e4613cd475b7e6449b5365f1697fddbc1378ce9a
BubbleTrouble-1 <madhushiva57@gmail.com>
2026-06-30 00:17:59 +0530

    Create Roman To Integer.java

---
+++ June29/Roman To Integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);map.put('V',5);map.put('X',10);map.put('L',50);
+        map.put('C',100);map.put('D',500);map.put('M',1000);
+
+        int n = s.length();
+        int roman=map.get(s.charAt(n-1));
+        for(int i=0;i<n-1;i++){
+            if(map.get(s.charAt(i))>=map.get(s.charAt(i+1))){
+                roman+=map.get(s.charAt(i));
+            }else{
+                roman-=map.get(s.charAt(i));
+            }
+        }
+        return roman;
+    }
+}
